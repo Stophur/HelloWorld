b0e5a140caa082966d47ed4ee14f6c2fd3411c5d
Chris <Stophur@gmail.com>
2017-11-12 10:44:46 -0700

    added loop

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,16 +1,26 @@
 import os
-a = os.getcwd()
-x="13 "
-y="BROKE"
-c = "HEY CHRIS, SORRY I BROKE YOUR STUFF"
+import time
+# a = os.getcwd()
+# x="13 "
+# y="BROKE"
+# c = "HEY CHRIS, SORRY I BROKE YOUR STUFF"
 
-print(x,y, c)
+# print(x,y, c)
 
-print ("test")
+# print ("test")
 
-x = "10"
-y = "fifteen"
+# x = "10"
+# y = "10"
 
-x=y
-print(x+y)
-print(int(x)+int(y))
+# x=y
+# print(x+y)
+# print(int(x)+int(y))
+
+# start = 1
+# temp = 1
+st=time.clock()
+for i in range(0,1000000):
+    # print(i)
+    temp = i
+et=time.clock()-st
+print (et)
